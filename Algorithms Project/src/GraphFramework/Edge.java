d22a6564e65f7308e1c454d12af960eb20df996c
qamaralashrafi <122994637+qamaralashrafi@users.noreply.github.com>
2023-05-10 00:34:42 +0300

    Update Edge.java

--- Algorithms Project/src/GraphFramework/Edge.java
+++ Algorithms Project/src/GraphFramework/Edge.java
@@ -1,15 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package GraphFramework;
 
+package GraphFramework;
 /**
  *
  * @author asil
+/*
+ *  @authors Asil, Qamar, Aroub,Khalida
+ * B9A
+ * CPCS-324
+ * Project Code
+ * 18th may. 2023
  */
-public class Edge {
+public class Edge implements Comparable<Edge> {
 
+    int weight;
+    Vertex source;
+    Vertex target;
+    Vertex parent;
     
-    
+    public Edge() {
+        source = new Vertex(); 
+        target = new Vertex(); 
+        parent = new Vertex(); 
+    }
+
+    public Edge(Vertex source, Vertex target, int weight) {
+        this.source = source;
+        this.target = target;
+        this.weight = weight;
+        parent= new Vertex();
+    }   
+
+    @Override
+    public int compareTo(Edge e) { // compare based on weights
+        if(this.weight > e.weight)
+            return 1;
+        else if (this.weight == e.weight)
+            return 0;
+        else return -1;}
+
+	public void displayInfo() {
+		
+	}
+	
 }
