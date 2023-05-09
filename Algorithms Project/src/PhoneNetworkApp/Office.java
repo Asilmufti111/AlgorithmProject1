06330dd534c021552076bc2794c5ef93072e0e54
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-05-09 21:58:16 +0300

    update office class #1

--- Algorithms Project/src/PhoneNetworkApp/Office.java
+++ Algorithms Project/src/PhoneNetworkApp/Office.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package PhoneNetworkApp;
 
-/**
- *
- * @author asil
+import GraphFramework.Vertex;
+
+
+/*
+ *  @authors Asil, Qamar, Arub,Khalida
+ * B9A
+ * CPCS-324
+ * Project Code
+ * 18th may. 2023
  */
-public class Office {
-    
-}
+public class Office extends Vertex {
+
+    public Office() {
+        setLabel();
+    }
+
+    // Methods
+    public void setLabel() {
+        super(label);
+
+    }
+
+    @Override
+    public void displayInfo() {
+        // 
+        System.out.print("The lable is:" + label);
+    } // End of method
+
+} // End of class
+
