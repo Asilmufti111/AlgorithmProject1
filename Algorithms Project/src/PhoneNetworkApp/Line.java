de78a3d03a043df8034cb1f83f5a69880a21b12e
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-05-10 22:00:39 +0300

    Update Line.java

--- Algorithms Project/src/PhoneNetworkApp/Line.java
+++ Algorithms Project/src/PhoneNetworkApp/Line.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package PhoneNetworkApp;
 
-/**
- *
- * @author asil
- */
-public class Line {
-    
-}
+import GraphFramework.*;
+
+public class Line extends Edge {
+
+    private int Ilength;
+
+    public Line(Vertex source, Vertex target, int weight) {
+        super(source, target, weight);
+        this.Ilength = weight * 5;
+    }
+
+    // Methods
+    @Override
+    public void displayInfo() {
+        // Output as required: road name: road x1 road size: 20
+        System.out.print("line length: " + getIlength());
+    } // End of Method
+
+    public int getIlength() {
+        return Ilength;
+    }
+
+} // End of Class
