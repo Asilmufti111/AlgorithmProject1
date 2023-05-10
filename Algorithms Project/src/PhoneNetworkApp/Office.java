085c06da649761edcee33ba7f3c47322dd6ad9f1
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-05-10 22:01:14 +0300

    Update Office.java

--- Algorithms Project/src/PhoneNetworkApp/Office.java
+++ Algorithms Project/src/PhoneNetworkApp/Office.java
@@ -12,20 +12,20 @@ import GraphFramework.Vertex;
  */
 public class Office extends Vertex {
 
-    public Office() {
-        setLabel();
+    public Office(String label) {
+        super(label);
     }
 
     // Methods
-    public void setLabel() {
-        super(label);
-
+    @Override
+    public void setLabel(String label) {
+        super.setLabel(label);
     }
 
     @Override
     public void displayInfo() {
         // 
-        System.out.print("The lable is:" + label);
+        System.out.print("The lable is:" + super.getLabel());
     } // End of method
 
 } // End of class
