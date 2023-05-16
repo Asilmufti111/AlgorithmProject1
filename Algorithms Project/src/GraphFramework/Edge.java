e0ea417e70cff538615846fca596f8453fd231d4
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-05-16 22:04:45 +0300

    Update Edge.java

--- Algorithms Project/src/GraphFramework/Edge.java
+++ Algorithms Project/src/GraphFramework/Edge.java
@@ -1,8 +1,6 @@
-
 package GraphFramework;
-/**
- *
- * @author asil
+
+
 /*
  *  @authors Asil, Qamar, Aroub,Khalida
  * B9A
@@ -16,30 +14,36 @@ public class Edge implements Comparable<Edge> {
     Vertex source;
     Vertex target;
     Vertex parent;
-    
+
     public Edge() {
-        source = new Vertex(); 
-        target = new Vertex(); 
-        parent = new Vertex(); 
+        source = new Vertex();
+        target = new Vertex();
+        parent = new Vertex();
     }
 
     public Edge(Vertex source, Vertex target, int weight) {
         this.source = source;
         this.target = target;
         this.weight = weight;
-        parent= new Vertex();
-    }   
+    }
+
+    public int parent(int pos) { // return the parent of the edge's node in the heap
+        return pos / 2;
+    }
 
     @Override
     public int compareTo(Edge e) { // compare based on weights
-        if(this.weight > e.weight)
+        if (this.weight > e.weight) {
             return 1;
-        else if (this.weight == e.weight)
+        } else if (this.weight == e.weight) {
             return 0;
-        else return -1;}
+        } else {
+            return -1;
+        }
+    }
 
-	public void displayInfo() {
-		
-	}
-	
+    public void displayInfo() {
+
+    }
+
 }
