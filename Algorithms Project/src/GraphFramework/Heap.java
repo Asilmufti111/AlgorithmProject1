690ca7a1c58e45d3609e1091f34dfc14d278b920
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-05-19 22:45:10 +0300

    Update Heap.java

--- Algorithms Project/src/GraphFramework/Heap.java
+++ Algorithms Project/src/GraphFramework/Heap.java
@@ -7,37 +7,55 @@ package GraphFramework;
  * Project Code
  * 18th may. 2023
  */
-// Java Program to Implement Heaps by Illustrating Min edges
-public class Heap {
+// Java Program to Implement Heaps
+// by Illustrating Min Heap
+class Heap {
 
-    // variables of this class
-    Graph graph=new Graph();
-    Edge[] edges;
-    int size = 0;
+    // Member variables of this class
+    Edge[] Heap;
+    int size;
+    int maxsize;
 
     // Initializing front as static with unity
-    private static final Edge root=new Edge();
+    static final int FRONT = 1;
 
     // Constructor of this class
-    public Heap() {
+    public Heap(int maxsize) {
+
         // This keyword refers to current object itself
+        this.maxsize = maxsize;
         this.size = 0;
-        edges = new Edge[graph.verticesNo + 1];;
-        root.weight = Integer.MIN_VALUE;
+
+        Heap = new Edge[this.maxsize + 1];
+        Heap[0] = new Edge();
+
+        Heap[0].weight = Integer.MIN_VALUE;
     }
 
+    // Method 1
+    // Returning the position of
+    // the parent for the node currently
+    // at pos
+    private int parent(int pos) {
+        return pos / 2;
+    }
+
+    // Method 2
     // Returning the position of the
     // left child for the node currently at pos
     private int leftChild(int pos) {
         return (2 * pos);
     }
 
+    // Method 3
     // Returning the position of
-    // the right child for the node currently at pos
+    // the right child for the node currently
+    // at pos
     private int rightChild(int pos) {
         return (2 * pos) + 1;
     }
 
+    // Method 4
     // Returning true if the passed
     // node is a leaf node
     private boolean isLeaf(int pos) {
@@ -49,15 +67,18 @@ public class Heap {
         return false;
     }
 
-    // To swap two nodes of the edges
+    // Method 5
+    // To swap two nodes of the heap
     private void swap(int fpos, int spos) {
 
-        int tmp;
-        tmp = edges[fpos].weight;
-        edges[fpos] = edges[spos];
-        edges[spos].weight = tmp;
+        Edge tmp;
+        tmp = Heap[fpos];
+
+        Heap[fpos] = Heap[spos];
+        Heap[spos] = tmp;
     }
 
+    // Method 6
     // To heapify the node at pos
     private void minHeapify(int pos) {
         if (!isLeaf(pos)) {
@@ -66,12 +87,12 @@ public class Heap {
             // to check if right child exists. Otherwise default value will be '0'
             // and that will be swapped with parent node.
             if (rightChild(pos) <= size) {
-                swapPos = edges[leftChild(pos)].weight < edges[rightChild(pos)].weight ? leftChild(pos) : rightChild(pos);
+                swapPos = Heap[leftChild(pos)].weight < Heap[rightChild(pos)].weight ? leftChild(pos) : rightChild(pos);
             } else {
                 swapPos = leftChild(pos);
             }
 
-            if (edges[pos].weight > edges[leftChild(pos)].weight || edges[pos].weight > edges[rightChild(pos)].weight) {
+            if (Heap[pos].weight > Heap[leftChild(pos)].weight || Heap[pos].weight > Heap[rightChild(pos)].weight) {
                 swap(pos, swapPos);
                 minHeapify(swapPos);
             }
@@ -79,44 +100,47 @@ public class Heap {
         }
     }
 
-    // To insert a node into the edges
+    // Method 7
+    // To insert a node into the heap
     public void insert(Edge element) {
 
-        if (size >= edges.length) {
+        if (size >= maxsize) {
             return;
         }
 
-        edges[size] = element;
+        Heap[++size] = element;
         int current = size;
 
-        while (edges[current].weight < edges[edges[current].parent(current)].weight) {
-            swap(current, edges[current].parent(current));
-            current = edges[current].parent(current);
-            size++;
+        while (Heap[current].weight < Heap[parent(current)].weight) {
+            swap(current, parent(current));
+            current = parent(current);
         }
     }
 
-    // To print the contents of the edges
-    public void displayInfo() {
+    // Method 8
+    // To print the contents of the heap
+    public void print() {
         for (int i = 1; i <= size / 2; i++) {
 
             // Printing the parent and both childrens
-            System.out.print(" PARENT : " + edges[i]
-                    + " LEFT CHILD : " + edges[2 * i]
-                    + " RIGHT CHILD :" + edges[2 * i + 1]);
+            System.out.print(
+                    " PARENT : " + Heap[i]
+                    + " LEFT CHILD : " + Heap[2 * i]
+                    + " RIGHT CHILD :" + Heap[2 * i + 1]);
 
             // By here new line is required
             System.out.println();
         }
     }
 
+    // Method 9
     // To remove and return the minimum
-    // element from the edges
+    // element from the heap
     public Edge remove() {
 
-        Edge popped = edges[root.weight];
-        edges[root.weight] = edges[size--];
-        minHeapify(root.weight);
+        Edge popped = Heap[FRONT];
+        Heap[FRONT] = Heap[size--];
+        minHeapify(FRONT);
 
         return popped;
     }
@@ -126,4 +150,3 @@ public class Heap {
     }
 
 }
-
