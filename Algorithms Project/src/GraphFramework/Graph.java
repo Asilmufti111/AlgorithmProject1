3451c229ce30eaca7e74b76590934d31aeb8ca4c
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-05-19 22:44:44 +0300

    Update Graph.java

--- Algorithms Project/src/GraphFramework/Graph.java
+++ Algorithms Project/src/GraphFramework/Graph.java
@@ -2,6 +2,7 @@ package GraphFramework;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 /*
@@ -15,52 +16,56 @@ public class Graph {
 
     int verticesNo;
     int edgeNo;
-    Boolean isDigraph;
-    Vertex[] vertices;
-    Graph map;
-    
+    Boolean isDigraph = false;
+    LinkedList< Vertex> vertices = new LinkedList<>();
 
     public Graph(int verticesNo, int edgeNo, boolean isDigraph) {
-        this.verticesNo = verticesNo;
-        this.edgeNo = edgeNo;
         this.isDigraph = isDigraph;
-        this.vertices = new Vertex[verticesNo];
     }
 
     public Graph() {
     }
 
-    public void makeGraph(int verticesNo, int edgeNo) {
+    public void makeGraph(int Vnum, int Enum) {
 
-        for (int i = 0; i<verticesNo;i++) {
-            vertices[i] = createVertex(i+"");
+        for (int i = 0; i < Vnum; i++) {
+            vertices.add(createVertex(i + ""));
+            verticesNo++;
+
         } //store all vertices 
 
-        for (int i = 0; i < verticesNo - 1; i++) { //at least connect all vertices
+        for (int i = 0; i < Vnum - 1; i++) { //at least connect all vertices
             //addEdge(source vertex, target vertex, random weight)	
             //ensure last vertex is only a target and not a source vertex
-            addEdge(vertices[i].label, vertices[i + 1].label, (int) (1 + Math.random() * 10));
+            addEdge(vertices.get(i), vertices.get(i + 1), (int) (1 + Math.random() * 10));
+            if (i == Vnum - 2) {
+                addEdge(vertices.get(Vnum - 1), vertices.get(0), (int) (1 + Math.random() * 10));
+
+            }
+
         }
 
         // fill rest of edges (to comly with number of edges in file)
-        for (int i = 0; i < (edgeNo - (verticesNo - 1)); i++) {
-            int U = (int) (Math.random() * verticesNo); //random source vertex
-            int V = (int) (Math.random() * verticesNo); //random target vertex
+        for (int i = 0; i < (Enum - (Vnum)); i++) {
+            int sourceInd; //random source vertex
+            int targetInd;
 
-            if (U != V) {//if source not equal target
+            do {
+                sourceInd = (int) (Math.random() * Vnum); //random source vertex
+                targetInd = (int) (Math.random() * Vnum); //random target vertex
+                if (sourceInd != targetInd) {
+                    for (int j = 0; j < vertices.get(sourceInd).adjList.size(); j++) { //go through source adjacency list to check all edges
 
-                for (int j = 0; j < vertices[U].adjList.size(); j++) { //go through source adjacency list to check all edges
-
-                    if (!(vertices[U].adjList.get(j).target.label.equals(V))) //if none of it is targets are equal to current target
-                    {
-                        break;
-                    }//if source and target don't have an edge break out of loop
+                        if (!(vertices.get(sourceInd).adjList.get(j).target.label.equals(vertices.get(targetInd).label))) //if none of it is targets are equal to current target
+                        {
+                            break;
+                        }//if source and target don't have an edge break out of loop
+                    }
                 }
-            } else {
-                continue; //if source == target go through next iteration 
-            }
+            } while (sourceInd == targetInd);
+
             //if soure and target don't have an edge and they are not equal, create new edge
-            addEdge(vertices[U].label, vertices[V].label, (int) (1 + Math.random() * 10));
+            addEdge(vertices.get(sourceInd), vertices.get(targetInd), (int) (1 + Math.random() * 10));
 
         }
     }
@@ -79,17 +84,11 @@ public class Graph {
         int All_Vertices = scan.nextInt(); // read and store number of vertice in total 
         int All_Edges = scan.nextInt(); // read and store number of edges in total 
 
-        if (!isDigraph) { //if graph is undirected     
-            All_Edges *= 2;
-        } //the edges go from source to target and from target to source
-
-        vertices = new Vertex[All_Vertices]; // Create array of verices to store all vertices 
-
         while (edgeNo < All_Edges) {
-            String v = (scan.next().charAt(0)) - 65 + ""; //read source label and assign it a number e.g A-->1 B-->2 ...etc.
-            String u = (scan.next().charAt(0)) - 65 + ""; // read target label and assign it a number 
-            int w = scan.nextInt();//read weight of edge between source and target vertices        
-            addEdge(v, u, w); //add edge between source and target vertex with specific weight 
+            String source = (scan.next().charAt(0)) - 65 + ""; //read source label and assign it a number e.g A-->1 B-->2 ...etc.
+            String target = (scan.next().charAt(0)) - 65 + ""; // read target label and assign it a number 
+            int weight = scan.nextInt();//read weight of edge between source and target vertices        
+//            addEdge(source, target, weight); //add edge between source and target vertex with specific weight 
 
         }
         scan.close();
@@ -104,42 +103,22 @@ public class Graph {
         return new Edge(v, u, w);
     }
 
-    public Edge addEdge( String v, String u, int w) {
+    public Edge addEdge(Vertex v, Vertex u, int w) {
 
-        Graph map = new Graph();
-        
-        int intV = Integer.parseInt(v); //typecast source label (String->int) to be able to access its index in vertices[]
+        Edge newEdge = createEdge(vertices.get(vertices.indexOf(v)), vertices.get(vertices.indexOf(u)), w); //create new edge object between v and u 
 
-        int intU = Integer.parseInt(u); //typecast target label (String->int) to be able to access its index in vertices[]
-
-        if (vertices[intV] == null) { 	 //if source vertex is not in array 	
-
-            verticesNo++;           //increment no of vertices
-
-            vertices[intV] = map.createVertex(v); // add this source vertex in vertice[]
-        }
-
-        if (vertices[intU] == null) {//if target vertex is not in array 	
-            verticesNo++;  //increment no of vertices
-            vertices[intU] = map.createVertex(u); // add this target vertex in vertice[]     		
-        }
-
-        Edge createEdge = map.createEdge(vertices[intV], vertices[intU], w); //create new edge object between source and target 
-
+        vertices.get(vertices.indexOf(v)).adjList.add(newEdge); //add this edge to the adjacency list of the v vertex
         edgeNo++; //increment no of edegs
-
-        vertices[intV].adjList.add(createEdge); //add this edge to the adjacency list of the source vertex
 
         if (!isDigraph) { //if graph is undirected 	
 
-            createEdge = map.createEdge(vertices[intU], vertices[intV], w);  //add edge from target to source (opposite way)  		 
+            newEdge = createEdge(vertices.get(vertices.indexOf(u)), vertices.get(vertices.indexOf(v)), w);  //add edge from u to v (opposite way)  		 
+            vertices.get(vertices.indexOf(u)).adjList.add(newEdge); //add this edge to the adjacency list of the v vertex
+            edgeNo++; //increment no of edegs
 
-            edgeNo++; // increment no of edegs
-
-            vertices[intU].adjList.add(createEdge);//add this edge to the adjacency list of the target vertex
         }
 
-        return createEdge; //if edge created, created edge will be returned by method
+        return newEdge; //if edge created, created edge will be returned by method
 
     }
 }
