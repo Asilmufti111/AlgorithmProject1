b9970a3ed5db720dd1448531c23bceae5047c367
qamaralashrafi <122994637+qamaralashrafi@users.noreply.github.com>
2023-05-28 18:15:59 +0300

    Update Graph.java

--- Algorithms Project/src/GraphFramework/Graph.java
+++ Algorithms Project/src/GraphFramework/Graph.java
@@ -1,9 +1,10 @@
 package GraphFramework;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /*
  *  @authors Asil, Qamar, Aroub,Khalida
@@ -12,157 +13,118 @@ import java.util.Scanner;
  * Project Code
  * 18th may. 2023
  */
-public class Graph {
 
-    int verticesNo;
-    int edgeNo;
-    Boolean isDigraph = false;
-    ArrayList<Vertex> vertices = new ArrayList<>();
+public class MHPrimAlg extends MSTAlgorithm {
+	
+	// Data fields
+	private int cost = 0;
+                
+                
+                Map<String,Integer> vertexVal;
 
-    public Graph(int verticesNo, int edgeNo, boolean isDigraph) {
-        this.verticesNo=verticesNo;
-        this.edgeNo=edgeNo;
-        this.isDigraph = isDigraph;
-        makeGraph();
-    }
-    public Graph(File graphFile) throws FileNotFoundException{
-        readGraphFromFile(graphFile);
-    }
+    // Stores the Minimum spanning Tree
+                List<Edge> result;
+	
+	/**
+	 * PQPrimAlg Constructor
+	 * @param graph
+	 */
+	public MHPrimAlg(Graph graph) {
+            
+        	MSTresultList = new LinkedList<>(); // Array holds the edges of MST
 
-    public Graph() {
-    }
+	}
+	
+	
+	/**
+	 * Shows Resulting MST
+     * @param graph
+	 */
 
-    public void makeGraph() {
-  
-        
-        for (int i = 0; i < verticesNo; i++) {
-            vertices.add(createVertex(i + ""));} //store all vertices 
+        @Override
+	public void findMST(Graph graph) {
+            vertexVal = new LinkedHashMap<>();
 
-        for (int i = 0; i < verticesNo - 1; i++) { //at least connect all vertices
-            //addEdge(source vertex, target vertex, random weight)	
-            //ensure last vertex is only a target and not a source vertex
-            addEdge(vertices.get(i), vertices.get(i + 1), (int) (1 + Math.random() * 10));
-            if (i == verticesNo - 2) {
-                addEdge(vertices.get(verticesNo - 1), vertices.get(0), (int) (1 + Math.random() * 10));
+        // Stores the Minimum spanning Tree
+           
+            
+            vertexVal = new LinkedHashMap<>();
 
+        // Vertex to Edge Map
+        Map<String, Edge> vertexToEdge = new HashMap<>();
+
+        // Assign all the initial values as infinity for all the Vertices.
+        for(int i = 0 ;i<graph.verticesNo;i++){
+            String v=graph.vertices.get(i).getLabel();
+            vertexVal.put(v,Integer.MAX_VALUE);//initialize to infinity (unknown)
+        }
+
+        MinHeap minHeap = new MinHeap(vertexVal);
+
+        // Call buildHeap() to create the MinHeap
+        minHeap.buildHeap();
+
+        // Replace the value of start vertex to 0.
+        minHeap.updateHeap("0",0);
+
+        // Continue until the Min-Heap is not empty.
+        while(!minHeap.empty()){
+            // Extract minimum value vertex from Map in Heap
+           
+            String currentVertex = minHeap.deleteMin();
+            // Need to get the edge for the vertex and add it to the Minimum Spanning Tree..
+            // Just note, the edge for the source vertex will not be added.
+            Edge spanningTreeEdge = vertexToEdge.get(currentVertex);
+            if(spanningTreeEdge != null) {
+                MSTresultList.add(spanningTreeEdge);//add to MST 
+                cost+= spanningTreeEdge.getWeight();//add cost
             }
 
-        }
-
-        // fill rest of edges (to comly with number of edges in file)
-        for (int i = 0; i < (edgeNo - (verticesNo)); i++) {
-            int sourceInd; //random source vertex
-            int targetInd;
-
-            do {
-                sourceInd = (int) (Math.random() * verticesNo); //random source vertex
-                targetInd = (int) (Math.random() * verticesNo); //random target vertex
-                if (sourceInd != targetInd) {
-                    for (int j = 0; j < vertices.get(sourceInd).adjList.size(); j++) { //go through source adjacency list to check all edges
-
-                        if (!(vertices.get(sourceInd).adjList.get(j).target.label.equals(vertices.get(targetInd).label))) //if none of it is targets are equal to current target
-                        {
-                            break;
-                        }//if source and target don't have an edge break out of loop
-                    }
+            // Get all the adjacent vertices and iterate through them.
+            for(Edge edge : getEdges(currentVertex,graph)){
+            
+                String adjacentVertex = edge.getTarget().getLabel();
+                
+                // We check if adjacent vertex exist in 'Map in Heap' and length of the edge is with this vertex
+                // is greater than this edge length.
+                if(minHeap.containsVertex(adjacentVertex) && minHeap.getWeight(adjacentVertex) > edge.getWeight()){
+                
+                    // Replace the edge length with this edge weight.
+                    minHeap.updateHeap(adjacentVertex, edge.getWeight());
+                    
+                    vertexToEdge.put(adjacentVertex, edge);
                 }
-            } while (sourceInd == targetInd);
-
-            //if soure and target don't have an edge and they are not equal, create new edge
-            addEdge(vertices.get(sourceInd), vertices.get(targetInd), (int) (1 + Math.random() * 10));
-
+            }
         }
     }
 
-    public void readGraphFromFile(File fileName) throws FileNotFoundException {
+    List<Edge> getEdges(String vertex,Graph graph){
 
-        
-        Scanner scan = new Scanner(fileName); // Create Scanner object to read from our file called fileName 
+        List<Edge> edgeList = new LinkedList<>();
 
-        String graphClassification = scan.nextLine(); // what is the type of the graph (directed/undirected)
+       Vertex vv = graph.vertices.get(Integer.parseInt(vertex));
+                    
+	for(int j=0; j<vv.getAdjList().size() ; j++) {
+               
+            edgeList.add(vv.getAdjList().get(j));} 
+
+        return edgeList;
+    }
+
+	
         
-        if (graphClassification.equalsIgnoreCase("digraph 0")) { //if graph is digraph 0 
-            
-            isDigraph = false; // then it is undirected 
-        
-        } else if (graphClassification.equalsIgnoreCase("digraph 1")) {//if graph is digraph 1
-           
-            isDigraph = true; //// then it is directed 
+
+ @Override
+    public void displayResultingMST() {
+
+        for (int i = 0; i < MSTresultList.size(); i++) {
+            MSTresultList.get(i).displayInfo();
+
+            System.out.println();
         }
-
-        verticesNo = scan.nextInt(); // read and store number of vertice in total 
-        
-        edgeNo = scan.nextInt(); // read and store number of edges in total 
-
-        if(!isDigraph){
-        
-            edgeNo*=2;}
-                 
-        while(scan.hasNext()){
-
-            String source = (int)(scan.next().charAt(0)) -65 + ""; //read source label and assign it a number e.g A-->1 B-->2 ...etc.
-            
-            String target = (int)(scan.next().charAt(0)) - 65 + ""; // read target label and assign it a number 
-            
-            int weight = scan.nextInt();//read weight of edge between source and target vertices        
-            
-            addEdge(createVertex(source), createVertex(target), weight); //add edge between source and target vertex with specific weight 
-
-        }
-        scan.close();
+        System.out.println("The cost of designed phone network: " + this.cost);
 
     }
+	   
 
-    public Vertex createVertex(String label) {
-        return null;
-    }
-
-    public Edge createEdge(Vertex v, Vertex u, int w) {
-        return  null;
-    }
-
-    public Edge addEdge(Vertex v, Vertex u, int w) {
-
-        boolean srcfound=false; //2 flags
-        
-        boolean trgtfound=false;
-        
-        for(int i = 0; i<vertices.size();i++){ //go thro all vertices
-        
-            if(vertices.get(i).getLabel().equals(v.label)){ //if source found in vertices
-            
-                srcfound=true;
-                
-                v= vertices.get(i);} //assign v with source
-        
-            else if(vertices.get(i).getLabel().equals(u.label)){ //if u found in vertices
-            
-                trgtfound=true;
-                
-                u= vertices.get(i);}}//assign u with target 
-        
-        if(!srcfound){
-        
-            vertices.add(v);} //if source not found add to vertices
-                
-        if(!trgtfound){
-        
-            vertices.add(u);}//if target not found add to vertices
-        
-         Edge newEdge = createEdge(vertices.get(Integer.parseInt(v.getLabel())), vertices.get(Integer.parseInt(u.getLabel())), w); //create new edge object between v and u 
-
-        vertices.get(Integer.parseInt(v.getLabel())).adjList.add(newEdge); //add this edge to the adjacency list of the v vertex
-
-        if (!isDigraph) { //if graph is undirected 	
-
-            newEdge = createEdge(vertices.get(vertices.indexOf(u)), vertices.get(vertices.indexOf(v)), w);  //add edge from u to v (opposite way)  		 
-            
-            vertices.get(vertices.indexOf(u)).adjList.add(newEdge); //add this edge to the adjacency list of the v vertex
-
-        }
-
-        return newEdge; //if edge created, created edge will be returned by method
-
-    }
-}
-
+} 
