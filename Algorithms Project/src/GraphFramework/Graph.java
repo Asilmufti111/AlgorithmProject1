ddf5e0aecab02bf1a54f86ea529f0f89456930d5
qamaralashrafi <122994637+qamaralashrafi@users.noreply.github.com>
2023-06-04 17:02:30 +0300

    Update Graph.java

--- Algorithms Project/src/GraphFramework/Graph.java
+++ Algorithms Project/src/GraphFramework/Graph.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
- *  @authors Asil, Qamar, Aroub,Khalida
+ *  @authors Asil, Qamar, Aroub,Khalida,Huda
  * B9A
  * CPCS-324
  * Project Code
