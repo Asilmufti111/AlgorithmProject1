55a32dcaa3ca281d217be0ffa27041288f4f7f4b
Asilmufti111 <126459608+Asilmufti111@users.noreply.github.com>
2023-06-14 16:52:20 +0300

    Update BluePrintGraph.java

--- Algorithms Project/src/PhoneNetworkApp/BluePrintGraph.java
+++ Algorithms Project/src/PhoneNetworkApp/BluePrintGraph.java
@@ -41,7 +41,5 @@ public class BluePrintGraph extends Graph {
     public Edge createEdge(Vertex source, Vertex target, int weight) {
         return new Line(source, target, weight);
     }
-    public void displayInfo(){
-    displayInfo();}
 
 }
